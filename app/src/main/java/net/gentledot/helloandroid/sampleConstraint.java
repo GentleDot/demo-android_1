81f85d290c098dae2777b97edd02360a9208018d
gentledot <gentledot.wp@gmail.com>
2017-05-15 00:28:19 +0900

    Android Practice - add activity(ConstraintLayout)

---
+++ app/src/main/java/net/gentledot/helloandroid/sampleConstraint.java
@@ -0,0 +1,13 @@
+package net.gentledot.helloandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class sampleConstraint extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample_constraint);
+    }
+}
