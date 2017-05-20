5704374c827d078a9d25869f3a2e0feea6c718d0
gentledot <gentledot.wp@gmail.com>
2017-05-21 01:42:11 +0900

    Android Practice - permissionSet and Check (not below 22)

--- app/src/main/java/net/gentledot/helloandroid/SMSReceiveActivity.java
+++ app/src/main/java/net/gentledot/helloandroid/SMSReceiveActivity.java
@@ -1,11 +1,17 @@
 package net.gentledot.helloandroid;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class SMSReceiveActivity extends AppCompatActivity {
     EditText senderBox;
@@ -16,6 +22,20 @@ public class SMSReceiveActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive);
+
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS);
+
+        if(permissionCheck == PackageManager.PERMISSION_GRANTED){ // SMS 수신 권한 체크
+            Toast.makeText(this, "SMS 수신 권한 있음.", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "SMS 수신 권한 없음.", Toast.LENGTH_LONG).show();
+
+//            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECEIVE_SMS)){  // 권한 설명이 필요한지 여부
+//                Toast.makeText(this, "SMS 권한 설명 필요함.", Toast.LENGTH_LONG).show();
+//            } else {
+                ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.RECEIVE_SMS}, 22);
+//            }
+        }
 
         senderBox = (EditText) findViewById(R.id.smsSender);
         contentBox = (EditText) findViewById(R.id.smsContent);
@@ -38,6 +58,25 @@ public class SMSReceiveActivity extends AppCompatActivity {
         processIntent(intent);
 
         super.onNewIntent(intent);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch (requestCode){
+            case 22: {
+                if(grantResults.length > 0){
+                    if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                        Toast.makeText(this, "SMS 수신 권한을 사용자가 승인함.", Toast.LENGTH_LONG).show();
+                    } else if(grantResults[0] == PackageManager.PERMISSION_DENIED){
+                        Toast.makeText(this, "SMS 수신 권한을 사용자가 거부함", Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(this, "SMS 수신 권한을 부여받지 못함.", Toast.LENGTH_LONG).show();
+                }
+            }
+
+        }
     }
 
     private void processIntent(Intent intent) {
