a5ae672ea9a4ac991f4da38edc48526865f86863
gentledot <gentledot.wp@gmail.com>
2017-05-21 22:25:17 +0900

    Android Practice - touch event (with gesture)

--- app/src/main/java/net/gentledot/helloandroid/SampleEvent.java
+++ app/src/main/java/net/gentledot/helloandroid/SampleEvent.java
@@ -2,12 +2,14 @@ package net.gentledot.helloandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 
 public class SampleEvent extends AppCompatActivity {
     TextView textBox;
+    GestureDetector detector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +39,50 @@ public class SampleEvent extends AppCompatActivity {
             }
         });
 
+        detector = new GestureDetector(this, new GestureDetector.OnGestureListener() {
+            @Override
+            public boolean onDown(MotionEvent e) {
+                println("onDown() 호출됨.");
+                return true;
+            }
+
+            @Override
+            public void onShowPress(MotionEvent e) {
+                println("onShowPress() 호출됨.");
+            }
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                println("onSingleTapUp() 호출됨.");
+                return true;
+            }
+
+            @Override
+            public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+                println("onScroll() 호출됨. : " + distanceX + ", " + distanceY );
+                return true;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent e) {
+                println("onLongPress() 호출됨.");
+            }
+
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                println("onFling() 호출됨. : " + velocityX + ", " + velocityY);
+                return true;
+            }
+        });
+
+        View view2 = findViewById(R.id.view2);
+        view2.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                detector.onTouchEvent(event);
+                return true;
+            }
+        });
     }
 
     private void println(String s) {
