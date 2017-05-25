7d531a911881f433bafb160cc2cf00f6eb9ac6f4
gentledot <gentledot.wp@gmail.com>
2017-05-25 11:52:38 +0900

    Android Practice - searchText in actionBar

--- app/src/main/java/net/gentledot/helloandroid/OptionMenu.java
+++ app/src/main/java/net/gentledot/helloandroid/OptionMenu.java
@@ -3,11 +3,18 @@ package net.gentledot.helloandroid;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class OptionMenu extends AppCompatActivity {
+    EditText editText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,16 @@ public class OptionMenu extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        View view = menu.findItem(R.id.menu_search).getActionView();
+
+        if(view != null){
+            editText = (EditText) view.findViewById(R.id.searchEditText);
+
+            if (editText != null){
+                editText.setOnEditorActionListener(onSearchListener);
+            }
+        }
         return true;
     }
 
@@ -43,4 +60,29 @@ public class OptionMenu extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    // 텍스트뷰에 키 입력이 끝날 때 이벤트 설정
+    TextView.OnEditorActionListener onSearchListener = new TextView.OnEditorActionListener() {
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+            if(event == null || event.getAction() == KeyEvent.ACTION_UP){
+                // 검색 메소드 호출
+                search();
+
+                // 키패드 닫기기
+               InputMethodManager inputManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                inputManager.hideSoftInputFromWindow(v.getWindowToken(), 0);
+            }
+
+            return true;
+        }
+    };
+
+    // 검색 메소드
+    private void search() {
+        String searchString = editText.getEditableText().toString();
+        Toast.makeText(this, "검색한 문자열 : " + searchString, Toast.LENGTH_LONG).show();
+    }
+
+
 }
