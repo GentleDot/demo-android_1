4726961ee00475594d2974986f42ba7f3e083a59
gentledot <gentledot.wp@gmail.com>
2017-06-02 11:15:29 +0900

    Android Practice - drawRectangle

---
+++ app/src/main/java/net/gentledot/helloandroid/DrawRectangleActivity.java
@@ -0,0 +1,13 @@
+package net.gentledot.helloandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DrawRectangleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_draw_rectangle);
+    }
+}
---
+++ app/src/main/java/net/gentledot/helloandroid/MainActivity.java
@@ -0,0 +1,24 @@
+package net.gentledot.helloandroid;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button rectangleBtn = (Button) findViewById(R.id.drawBtn);
+        rectangleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), DrawRectangleActivity.class));
+            }
+        });
+    }
+}
