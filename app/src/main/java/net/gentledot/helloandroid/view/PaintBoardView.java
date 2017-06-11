e91718464feaae66627d432edf7f8e3766e5cbf9
gentledot <gentledot.wp@gmail.com>
2017-06-11 19:24:53 +0900

    Android Practice - goodPaintBoard : changed draw (drawLine -> drawPath)

--- app/src/main/java/net/gentledot/helloandroid/view/PaintBoardView.java
+++ app/src/main/java/net/gentledot/helloandroid/view/PaintBoardView.java
@@ -1,10 +1,7 @@
 package net.gentledot.helloandroid.view;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
+import android.graphics.*;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -18,18 +15,27 @@ import java.util.Stack;
  */
 public class PaintBoardView extends View{
     private static final String TAG = "PaintBoardView";
+    private static final float TOUCH_TOLERANCE = 8;
+
+    private final Path mPath = new Path();
+
 
     Stack undos = new Stack();
     public static int maxUndos = 10;
 
     public boolean changed = false;
 
-    Paint paint;
-    Bitmap mBitmap;
-    Canvas mCanvas;
+    private Paint paint;
+    private Bitmap mBitmap;
+    private Canvas mCanvas;
 
-    float prevX;
-    float prevY;
+    private float prevX;
+    private float prevY;
+
+    private float mCurveEndX;
+    private float mCurveEndY;
+
+    private int mInvalidateExtraBorder = 10;
 
 
 
@@ -68,11 +74,19 @@ public class PaintBoardView extends View{
         float curX = event.getX();
         float curY = event.getY();
 
+        Rect rect = null;
+
         switch (action){
             case MotionEvent.ACTION_DOWN:
                 saveUndo();
 
-                if(prevX != -1){
+                rect = touchDown(event);
+
+                if(rect != null){
+                    invalidate(rect);
+                }
+
+                /*if(prevX != -1){
                     if (curX != prevX || curY != prevY){
                         mCanvas.drawLine(prevX, prevY, curX, curY, paint);
                     }
@@ -80,28 +94,49 @@ public class PaintBoardView extends View{
 
                 prevX = curX;
                 prevY = curY;
-                break;
+                break;*/
+
+                return true;
             case MotionEvent.ACTION_MOVE:
-                if(prevX >= 0 || prevY >= 0){
+                rect = touchMove(event);
+
+                if(rect != null){
+                    invalidate(rect);
+                }
+
+                return true;
+
+                /*if(prevX >= 0 || prevY >= 0){
                     mCanvas.drawLine(prevX, prevY, curX, curY, paint);
                 }
 
                 prevX = curX;
                 prevY = curY;
-                break;
+                break;*/
             case MotionEvent.ACTION_UP:
                 changed = true;
-                prevX = -1;
+
+                rect = touchUp(event, false);
+
+                if(rect != null){
+                    invalidate(rect);
+                }
+
+                mPath.rewind();
+                return true;
+
+               /* prevX = -1;
                 prevY = -1;
 
-                break;
+                break;*/
             default:
                 break;
 
         }
 
         invalidate();
-        return true;
+//        return true;
+        return false;
     }
 
     public void setColor(int color){
@@ -181,8 +216,16 @@ public class PaintBoardView extends View{
 
     private void init(Context context) {
         paint = new Paint();
-        paint.setColor(Color.BLACK);
-        paint.setStrokeWidth(2);
+        /*paint.setColor(Color.BLACK);
+        paint.setStrokeWidth(2);*/
+
+        paint.setAntiAlias(true);
+        paint.setColor(0xFF000000);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeJoin(Paint.Join.ROUND);
+        paint.setStrokeCap(Paint.Cap.ROUND);
+        paint.setStrokeWidth(2.0F);
+        paint.setDither(true);
 
         prevX = -1;
         prevY = -1;
@@ -237,6 +280,72 @@ public class PaintBoardView extends View{
         mCanvas = canvas;
 
         clearUndo();
+    }
+
+    private Rect touchDown(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+
+        prevX = x;
+        prevY = y;
+
+        Rect mInvalidRect = new Rect();
+        mPath.moveTo(x, y);
+
+        final int border = mInvalidateExtraBorder;
+        mInvalidRect.set((int) x - border, (int) y - border, (int) x + border, (int) y + border);
+
+        mCurveEndX = x;
+        mCurveEndY = y;
+
+        mCanvas.drawPath(mPath, paint);
+
+        return mInvalidRect;
+    }
+
+    private Rect touchUp(MotionEvent event, boolean b) {
+        Rect rect = processMove(event);
+
+        return rect;
+    }
+
+    private Rect touchMove(MotionEvent event) {
+        Rect rect = processMove(event);
+
+        return rect;
+    }
+
+    private Rect processMove(MotionEvent event){
+        final float x = event.getX();
+        final float y = event.getY();
+
+        final float dx = Math.abs(x - prevX);
+        final float dy = Math.abs(y - prevY);
+
+        Rect mInvalidRect = new Rect();
+
+        if(dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE){
+            final int border = mInvalidateExtraBorder;
+            // 다시 그려질 영역으로 현재 이동한 좌표 추가
+            mInvalidRect.set((int) mCurveEndX - border, (int) mCurveEndY - border, (int) mCurveEndX + border, (int) mCurveEndY + border);
+
+            float cX = mCurveEndX = (x + prevX) / 2F;
+            float cY = mCurveEndY = (y + prevY) / 2F;
+
+            // Path 객체에 현재 좌표값을 곡선으로 추가
+            mPath.quadTo(prevX, prevY, cX, cY);
+
+            mInvalidRect.union((int) prevX - border, (int) prevY - border, (int) prevX + border, (int) prevY + border);
+            mInvalidRect.union((int) cX - border, (int) cY - border, (int) cX + border, (int) cY + border);
+
+            prevX = x;
+            prevY = y;
+
+            mCanvas.drawPath(mPath, paint);
+
+        }
+        return mInvalidRect;
+
     }
 
 
